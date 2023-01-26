94490e1b4cd3bc22478bfe26c2b737f9ec187623
5wintaek <elqkddl@naver.com>
2023-01-27 00:57:23 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.js
@@ -0,0 +1,13 @@
+function solution(n) {
+    // 새로운 배열을 생성
+    const result = []; 
+    // 순차적으로 반복해야하니 for문 작성
+    for(let i=1; i<=n; i++){
+        // 2로 나눴을때 나머지가 1이 나오면 홀수
+        if(i%2 == 1){
+            // 새로운 배열에 push 
+            result.push(i)
+        }
+    }
+    return result
+}
