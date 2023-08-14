2c483bc0cb51f0da90106ced093b47043be0031c
Seung taek Oh <elqkddl@naver.com>
2023-08-14 23:58:40 +0900

    [Feat] 백준 3003 연습풀이

--- js.js
+++ js.js
@@ -1,8 +1,38 @@
-function solution(age) {
-  let birth = new Date();
-  let birthYear = birth.getFullYear("1983,01,01"); // 생일년도
-  let today = new Date("2022,01,01"); // 오늘날짜
-  let todayYear = today.getFullYear(); // 오늘년도
-  let year = todayYear - birthYear; // 오늘년도 - 생일년도 = 나이
-  return age;
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const input = [];
+rl.on("line", function (line) {
+  input.push(line);
+}).on("close", function () {
+  solution();
+  process.exit();
+});
+// a = 1 1 2 2 2 8
+// b = 0 1 2 2 2 7
+// c = 1 0 0 0 0 1
+
+function solution() {
+  const whiteChess = input[0].split(" ").map(Number);
+  const chess = [1, 1, 2, 2, 2, 8];
+  let answer = "";
+  let i = 0;
+  while (i < whiteChess.length) {
+    const item = chess[i] - whiteChess[i];
+    i++;
+    answer += item + " ";
+  }
+  console.log(answer);
 }
+
+// for (let i = 0; arr.length > i; i += 2) {
+//   arr[i] = "a";
+// }
+// let i = -1;
+// while (++i < arr.length) {
+//   arr[i] = "a";
+//   // console(arr[i]);
+// }
