95aeb10c2237e18bf0352baddec77a551cf5d368
5wintaek <elqkddl@gmail.com>
2023-11-14 21:11:56 +0900

    [level unrated] Title: 배열 두배 만들기, Time: 3.55 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120809. 배열 두배 만들기/배열 두배 만들기.js
@@ -0,0 +1,8 @@
+function solution(numbers) {
+    let doubledMap = numbers.map(el => el*2);
+    console.log(doubledMap)
+    return doubledMap
+}
+
+// map은 배열 요소 전체를 대상으로 함수를 호출
+// 함수 호출 결과를 배열로 반환
