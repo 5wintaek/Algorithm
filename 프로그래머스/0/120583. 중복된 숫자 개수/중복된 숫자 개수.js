896288ca1496fba8aa34cee704f5722b184a9b46
5wintaek <elqkddl@gmail.com>
2024-03-22 21:39:39 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.js
@@ -0,0 +1,6 @@
+function solution(array, n) {
+    // let answer = array.filter(a => a === n);
+    // return answer.length
+    const answer = array.filter(a => a === n).length;
+    return answer
+}
