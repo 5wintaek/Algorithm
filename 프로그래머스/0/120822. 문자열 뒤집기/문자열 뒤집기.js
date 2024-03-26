8504f03a06ee31e1062c5f6a714ca2dbfe2501d1
5wintaek <elqkddl@gmail.com>
2024-03-26 22:26:56 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.js
@@ -0,0 +1,9 @@
+function solution(my_string) {
+    return Array.from(my_string).reverse().join('')   
+}
+
+//split을 사용하지 않고 array.from 으로 새로운 배열 객체를 생성
+
+// reverse 를 사용하여 거꾸로뒤집어줌
+
+// join(””) 을 사용하여 새 문자열로 반환
