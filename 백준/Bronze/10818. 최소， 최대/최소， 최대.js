a59b4052f5997dd3fff8f943a7977d03ca5e6899
5wintaek <elqkddl@gmail.com>
2024-05-08 22:49:48 +0900

    [Bronze III] Title: 최소, 최대, Time: 496 ms, Memory: 87656 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.js
@@ -0,0 +1,20 @@
+const file = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = require("fs").readFileSync(file).toString().split("\n");
+
+let count = Number(input[0]);
+let numbers = input[1].split(" ").map((x) => Number(x));
+
+let max = numbers[0];
+let min = numbers[0];
+
+for (let i = 1; i < count; i++) {
+  if (max < numbers[i]) {
+    max = numbers[i];
+  }
+
+  if (min > numbers[i]) {
+    min = numbers[i];
+  }
+}
+
+console.log(`${min} ${max}`);
