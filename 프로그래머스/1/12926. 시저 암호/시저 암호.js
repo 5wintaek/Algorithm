3d1757ecc124f0ec1051999cb02600ac07303420
5wintaek <elqkddl@gmail.com>
2024-07-11 12:56:58 +0900

    [level 1] Title: 시저 암호, Time: 1.42 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.js
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.js
@@ -2,21 +2,18 @@ function solution(s, n) {
   const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   const lower = upper.toLowerCase();
 
-  return s
-    .split("")
-    .map((char) => {
-      if (char === " ") {
-        return " ";
+  return s.split("").map((char) => {
+      if(char === " "){
+          return " "
       }
-
       const charArr = upper.includes(char) ? upper : lower;
-
-      let index = charArr.indexOf(char) + n;
-      if (index >= charArr.length) {
-        index -= charArr.length;
+      
+      let index = charArr.indexOf(char) + n
+      
+      if(index >= charArr.length){
+          index -= charArr.length
       }
-
-      return charArr[index];
-    })
-    .join("");
+        return charArr[index]
+      
+  }).join("")
 }
