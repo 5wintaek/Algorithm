eda9efbd84f3d818bbba5c9299850e8c426d202d
5wintaek <elqkddl@gmail.com>
2024-10-02 00:12:47 +0900

    [level 0] Title: 짝수의 합, Time: 0.20 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.js
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.js
@@ -1,14 +1,15 @@
 function solution(n) {
-   // return Array(n).fill().map((_,i)=> i+1).filter( v=> v % 2 ===0).reduce((a,c) => a+c,0)
-    
-    let answer = 0;
-    for(let i=2; i<=n; i+=2){
-        answer += i
-        console.log(answer)
-    }
-    
-    return answer
+   return Array(n).fill().map((_,i)=> i+1).filter( v=> v % 2 ===0).reduce((a,c) => a+c,0)
 }
+    // reduce(a,c) 에서 a에는 초기값0이 들어가고 c 에는 배열 첫번째(2) 요소
+    // 두번쨰에서는 a는 2, c에는 4 가 들어가고 a에는 6이 들어감
+    // 세번째에서는 a는 6, c에는 6 이 들어가고 a에는 12 들어감
+//    let answer = 0 
+//    for(let i=0; i<=n; i++){
+//        if(i%2 === 0) answer += i;
+//    }
+//     return answer
+// }
 
 
 
