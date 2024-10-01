2aefceea93bf0c568e9544975d5512d26d9c1c1d
5wintaek <elqkddl@gmail.com>
2024-10-01 23:49:54 +0900

    [level 0] Title: 양꼬치, Time: 2.24 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.js
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.js
@@ -1,9 +1,13 @@
 function solution(n, k) {
-      // 양꼬치 n인분, 음료수 k개
-    if(n >= 10){
-        k -= Math.floor(n/10)
-    }
-    console.log(k)
-  
-    return 12000 * n + 2000 * k
+    if(n>=10){
+        k-= parseInt(n/10)
+        console.log(k)
+    } 
+    return 12000 * n + 2000 * k    
 }
+
+// k 양꼬치 : 12000
+// n 음료수 : 2000
+// 10인분을 먹으면 음료 + 1
+// n이 만약 10 이상이면 n/10 을 해서 값을 구함
+// 소수점이 찍히니 Math.floor를 사용 , parseInt 사용
