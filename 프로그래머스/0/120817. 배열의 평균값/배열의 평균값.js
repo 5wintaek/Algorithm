e46bf1bbc9f087d196c771d4f038a1c1f5f98295
5wintaek <elqkddl@gmail.com>
2024-10-02 00:43:30 +0900

    [level 0] Title: 배열의 평균값, Time: 6.74 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.js
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.js
@@ -1,11 +1,9 @@
 function solution(numbers) {
-   let answer = 0
-   for(let i=0; i<numbers.length; i++){
-       answer += (numbers[i])
-       // console.log(answer)
-   }
-    const sum = answer / numbers.length
-    console.log(sum)
-    return sum
+   let answer = 0;
+    for(num of numbers){
+        answer += num 
+        console.log(answer)
+    }
+    return answer / numbers.length
 
 }
