e3f39588bd8f745f92db573e7e408aa88f5b9ef0
5wintaek <elqkddl@gmail.com>
2025-01-09 22:32:23 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.py
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.py
@@ -1,9 +1,13 @@
+# # def solution(n):
+#     answer = 0 
+#     if n % 7 == 0 :
+#         answer = n // 7
+#     else :
+#         answer = n // 7 + 1
+        
+#     return answer
+
+import math
+
 def solution(n):
-    answer = 0 
-    if n % 7 == 0 :
-        answer = n // 7
-    else :
-        answer = n // 7 + 1
-        
-    return answer
-        
+    return math.ceil(n/7)
