2eb8b99610e8014afa8d1d23232dad167c41995b
5wintaek <elqkddl@gmail.com>
2025-01-22 21:48:36 +0900

    Title: 특정 조건의 온전수 구하기, Time: 39ms, Memory: 16MB, Status: Passed - CodeTree

--- 250122/특정 조건의 온전수 구하기/print-onjeonsu.py
+++ 250122/특정 조건의 온전수 구하기/print-onjeonsu.py
@@ -1,6 +1,12 @@
 n = int(input())
 
 for i in range(1,n+1):
-    if i % 2 == 0 or i % 5 == 0 or (i % 3 == 0 and i % 9 != 0):
+    if i % 2 == 0:
         continue
-    print(i, end = ' ')
+    elif i % 10 == 5 :
+        continue   
+    elif i % 3  == 0 and i % 9 != 0:
+        continue   
+    print(i,end=' ')
+    
+        
