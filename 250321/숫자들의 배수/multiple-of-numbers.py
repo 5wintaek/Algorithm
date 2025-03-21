541cc902cadcd28af92159ca6bf27f45e96b79b6
5wintaek <elqkddl@gmail.com>
2025-03-21 14:22:11 +0900

    Title: 숫자들의 배수, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250321/숫자들의 배수/multiple-of-numbers.py
@@ -0,0 +1,22 @@
+# n = int(input())
+
+# cnt = 0
+# val = 0
+
+# while cnt < 2 :
+#     val += n
+#     print(val, end=' ')
+#     if val % 5 == 0 :
+#         cnt += 1
+
+
+n = int(input())
+
+cnt = 0 
+value = 0 
+
+while cnt < 2 :
+    value += n 
+    print(value,end=' ')
+    if value % 5 == 0 :
+        cnt += 1
